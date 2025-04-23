b8c37546c410cd095b46a44f102395cb338d6e0b
vardhanreddy2003 <84281963+vardhanreddy2003@users.noreply.github.com>
2025-04-23 17:26:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,9 @@ def process_uploaded_image(file_obj):
     return img_array
 
 
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0',port=8080)
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
