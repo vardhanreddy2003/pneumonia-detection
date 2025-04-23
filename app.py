30477988f27396f5c74890b5502419c612c46d26
vardhanreddy2003 <vardhan16032003@gmail.com>
2025-04-23 08:25:13 +0530

    added model

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
+from tensorflow.keras.models import load_model
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = '0'
+
+model = load_model("pneumonia_detection_model.h5",compile=False)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def detect_result():
+    pneumonia_image = request.files.get("image")
+
+    if not pneumonia_image:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    processed_image = process_uploaded_image(pneumonia_image)
+    prediction = model.predict(processed_image,verbose=0)
+    
+    result=" "
+    
+    if(prediction[0]>0.5):
+        result="positive"
+    else:
+        result="negative"
+    return result        
+
+
+def process_uploaded_image(file_obj):
+    img = Image.open(file_obj).convert('RGB')
+    img = img.resize((256, 256))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)  # Shape: (1, 256, 256, 3)
+    return img_array
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8080)
